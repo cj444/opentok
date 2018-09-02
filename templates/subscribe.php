dfea54f372a2b37d6791824d52a9bce6bea08572
cj444 <42742847+cj444@users.noreply.github.com>
2018-09-02 17:23:51 +0530

    Add files via upload

--- templates/subscribe.php
+++ templates/subscribe.php
@@ -12,21 +12,17 @@ padding: 6px;
 width: 10%;
 font-size: 16px;}
 </style>
-<div id="login" style="width:100%;text-align:center;">
-<input type="text" id="roomname" style="width:20%;height:46px;" placeholder="Please Enter Room Name to subscribe a stream." >
-<button id="go" >Go</button>
-</div>
+
     <div id="videos">
          <div id="subscriber"></div> 
     </div>
     <script>
 	    var SERVER_BASE_URL = 'https://chetanphp.herokuapp.com';
-	    var buttongo = document.getElementById("go");
 	  
-	      buttongo.onclick = myFunction;
-    function myFunction() { 
-           var channel = document.getElementById("roomname").value;	
-      		fetch(SERVER_BASE_URL + '/room/'+channel).then(function(res) {
+	  
+	    myFunction('test');
+    function myFunction(channel) { 
+         	fetch(SERVER_BASE_URL + '/room/'+channel).then(function(res) {
 			  return res.json()
 			}).then(function(res) {
 			  apiKey = res.apiKey;
@@ -52,8 +48,8 @@ function initializeSession() {
    //alert(event.stream.streamId);
    session.subscribe(event.stream, 'subscriber', {
     insertMode: 'append',
-    width: '100%',
-    height: '100%'
+    width: '20%',
+    height: '20%'
   }, handleError); 
 }); 
 session.on("streamDestroyed", function (event) {
