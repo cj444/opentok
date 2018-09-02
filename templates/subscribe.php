df9652c069c85332cd802ac711723e79489793d4
cj444 <42742847+cj444@users.noreply.github.com>
2018-09-02 19:12:13 +0530

    Add files via upload

--- templates/subscribe.php
+++ templates/subscribe.php
@@ -49,39 +49,24 @@ font-size: 16px;}
 function initializeSession(channel) {
 
   var session = OT.initSession(apiKey, sessionId);
-
-  var connectionCount;
-session.on({
-  connectionCreated: function (event) {
-       console.log("existing stream in the session: " + event.stream.streamId);
-    connectionCount++;
-    if (event.connection.connectionId != session.connection.connectionId) {
-      console.log('Another client connected. ' + connectionCount + ' total.');
-    }
-  },
-  connectionDestroyed: function connectionDestroyedHandler(event) {
-    connectionCount--;
-    console.log('A client disconnected. ' + connectionCount + ' total.');
-  }
-});
-  
-  
+ 
+    session.connect(token, function(error) { 
+    });
   // Subscribe to a newly created stream
      session.on('streamCreated', function(event) {
-   console.log("New stream in the session: " + event.stream.streamId);
-   //alert(event.stream.streamId);
-   session.subscribe(event.stream, channel, {
-    insertMode: 'append',
-    width: '100%',
-    height: '50%'
-  }, handleError); 
-}); 
+		   console.log("New stream in the session: " + event.stream.streamId);
+		   //alert(event.stream.streamId);
+		   session.subscribe(event.stream, channel, {
+			insertMode: 'append',
+			width: '100%',
+			height: '50%'
+		  }, handleError); 
+		}); 
 session.on("streamDestroyed", function (event) {
   console.log("Stream stopped. Reason: " + event.reason);
 });
  
-  session.connect(token, function(error) { 
-  });
+
 }
 </script>
    
