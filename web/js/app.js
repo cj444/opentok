f6c8b9f7dbae512542dce41780aa407401a2792b
cj444 <42742847+cj444@users.noreply.github.com>
2018-09-02 14:13:10 +0530

    Add files via upload

--- web/js/app.js
+++ web/js/app.js
@@ -6,7 +6,7 @@ var token = "YOUR_TOKEN";
 // (optional) add server code here
 // (optional) add server code here
     var SERVER_BASE_URL = 'https://chetanphp.herokuapp.com';
-    fetch(SERVER_BASE_URL + '/session').then(function(res) {
+    fetch(SERVER_BASE_URL + '/room/'+channel).then(function(res) {
       return res.json()
     }).then(function(res) {
       apiKey = res.apiKey;
